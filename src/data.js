5175e9cb355cb57a9f27f714b9766771ff48b079
Adrian Kuśmierek <9018464@student.zadkine.nl>
2023-10-12 13:51:21 +0200

    Depoloy

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const projects = [
+    {
+      title: "Discord.JS Chatbot",
+      subtitle: "JavaScript & Node.JS",
+      description:
+        "A Chatbot on the Discord platform that I've made for a client with a Discord server consisting of over 300 thousand members.",
+      image: "./discord.jpeg",
+      link: "https://github.com/AdrianKusmierek/GoldenHelper",
+    },
+    {
+        title: "Discord.JS Chatbot",
+        subtitle: "JavaScript & Node.JS",
+        description:
+          "An older project of mine, but also the one that I got paid the most for. This was yet another Discord Chatbot written using the Discord.JS v12 Package.",
+        image: "./discord.jpeg",
+        link: "https://github.com/AdrianKusmierek/FW_Bot",
+      },
+    {
+      title: "C# Web Server",
+      subtitle: "C#",
+      description:
+        "A simple webserver I made in CSharp as a personal project to get more familiar with CSharp's Syntax and Libraries.",
+      image: "./cs.webp",
+      link: "https://github.com/AdrianKusmierek/CS-Web-Server",
+    },
+    {
+      title: "Tetrahedral Number Functions",
+      subtitle: "MASM, C++, C#, Java, JavaScript & Python",
+      description:
+        "Tetrahedral Number Functions written in multiple languages to see if I understand the basics enough to write such functions.",
+      image: "./coding.jpg",
+      link: "https://github.com/AdrianKusmierek/Tetrahedral-Number-Functions",
+    },
+  ];
+  
+export const skills = [
+  "Goed omgaan met kritiek",
+  "Ervaring met veel front-end frameworks",
+  "Goed samenwerken",
+  "Ervaring met Low Level Programmeertalen",
+  "Snel leren",
+  "Verstand van Linux/UNIX",
+];
