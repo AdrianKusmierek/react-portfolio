4e1c3f0e03f8358a970d3857b317e0b1c1bab80d
Adrian Kuśmierek <9018464@student.zadkine.nl>
2023-10-12 14:06:03 +0200

    netlify2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -64,10 +64,11 @@ export default function Contact() {
           </div>
         </div>
         <form
-          data-netlify="true"
           name="contact"
           onSubmit={handleSubmit}
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+          netlify
+        >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contacteer Me
           </h2>
